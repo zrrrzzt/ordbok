2b725a1c5266e1dc2efddba1b3010265613f77c1
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-10 17:45:50 +0200

    Adds test for paradigme

---
+++ test/testParadigme.js
@@ -0,0 +1,26 @@
+var paradigme = require('../lib/paradigme')
+  , assert = require('assert')
+  ;
+
+describe('Paradigme', function(){
+
+  it('Should return a list with 4 words', function(done){
+
+    var id = '50975'
+      ;
+
+    paradigme(id, function(err, list){
+
+      if(err){
+        throw err;
+      }
+
+      assert.equal(4, list.length);
+
+      done();
+
+    });
+
+  });
+
+});
