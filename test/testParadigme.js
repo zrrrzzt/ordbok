50b90fa042e3741e1cdb5812a19e940fcf5135de
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-11 14:16:53 +0200

    Adds validation of input and updates test

--- test/testParadigme.js
+++ test/testParadigme.js
@@ -9,9 +9,10 @@ describe('Paradigme', function(){
   it('Should return a list with 4 words', function(done){
 
     var id = '50975'
+      , lang = 'BM'
       ;
 
-    paradigme(id, function(err, list){
+    paradigme(id, lang, function(err, list){
 
       if(err){
         throw err;
